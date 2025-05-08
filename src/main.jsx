044759ef9ebe64c9d174c4aef55ac5f9bfc511d7
Keerthi-AI <keerthinathanofficial@gmail.com>
2025-05-08 16:04:45 +0530

    Initial portfolio upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom"
 import './index.css'
 
 // Improved Components
@@ -11,23 +11,15 @@ import Project from './components/Project'
 import Contact from './components/Contact'
 
 // Root component
-function App() {
-  return (
-    <HashRouter>
-      <Navbar />
-      <main className="relative overflow-hidden">
-        <Home />
-        <About />
-        <Project />
-        <Contact />
-      </main>
-    </HashRouter>
-  );
-}
-
-// Create root and render app
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>
+    <BrowserRouter>
+      <Navbar />
+      <Home />
+      <About />
+      <Project />
+      <Contact />
+    </BrowserRouter>
+  </StrictMode>,
+
 )
